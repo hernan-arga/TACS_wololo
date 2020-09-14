fb7b1c4985adecc973a5c1ecda14b1b70b7d05fb
hernan-arga <hernan_rodriguez_2013@hotmail.com>
2020-09-13 21:55:18 -0300

    Agrego antoacion para CORS

--- src/main/java/tacs/wololo/controllers/UsersController.java
+++ src/main/java/tacs/wololo/controllers/UsersController.java
@@ -7,11 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.slf4j.Slf4j;
 import tacs.wololo.model.User;
@@ -20,6 +16,7 @@ import tacs.wololo.services.implementations.UserService;
 
 @Slf4j
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api")
 public class UsersController
 {
