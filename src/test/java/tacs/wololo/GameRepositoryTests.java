bce534225509b4f8960ddd569bd7a6e9c3247080
MelisaRodirguez <melisarodrig@hotmail.com>
2020-09-26 17:41:11 -0300

    Refactor GameRepositoryTests

--- src/test/java/tacs/wololo/GameRepositoryTests.java
+++ src/test/java/tacs/wololo/GameRepositoryTests.java
@@ -4,62 +4,68 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import tacs.wololo.model.DTOs.GameInfoDto;
+import tacs.wololo.model.Game;
 import tacs.wololo.repositories.GameRepository;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class GameRepositoryTests {
-    /*
+
     @Autowired
     private GameRepository gameRepository;
-    private GameInfoDto funnyGame;
-    private GameInfoDto mehGame;
+    private Game funnyGame;
+    private Game mehGame;
 
     @Before
     public void init()
     {
-        List<GameInfoDto> gamesInfoDtos = new ArrayList<>();
 
-        List<String> playersFunnyGame = Stream.of("Lucia", "Carolina", "Cristian", "Sebastian").collect(Collectors.toList());
-        List<String> playersBoringGame = Stream.of("Lucia", "Cristian").collect(Collectors.toList());
-        List<String> playersMehGame = Stream.of("Lucia", "Carolina", "Sebastian").collect(Collectors.toList());
+        Queue<String> playersFunnyGame =new LinkedList<String>();
+        Queue<String> playersBoringGame =new LinkedList<String>();
+        Queue<String> playersMehGame =new LinkedList<String>();
 
-        funnyGame = new GameInfoDto(playersFunnyGame, "Chaco",25);
-        GameInfoDto boringGame = new GameInfoDto(playersBoringGame, "Tierra del Fuego",3);
-        mehGame = new GameInfoDto(playersMehGame, "Chaco",20);
+        playersFunnyGame.add("Lucia"); playersFunnyGame.add("Carolina");
+        playersFunnyGame.add("Cristian"); playersFunnyGame.add("Sebastian");
 
-        gamesInfoDtos.add(funnyGame);
-        gamesInfoDtos.add(boringGame);
-        gamesInfoDtos.add(mehGame);
+        playersBoringGame.add("Lucia"); playersBoringGame.add("Cristian");
+
+        playersMehGame.add("Lucia"); playersMehGame.add("Carolina");
+        playersMehGame.add("Sebastian");
+
+        //List<String> playersFunnyGame = Stream.of("Lucia", "Carolina", "Cristian", "Sebastian").collect(Collectors.toList());
+        //List<String> playersBoringGame = Stream.of("Lucia", "Cristian").collect(Collectors.toList());
+        //List<String> playersMehGame = Stream.of("Lucia", "Carolina", "Sebastian").collect(Collectors.toList());
+
+        //
+        funnyGame = new Game();
+        Game boringGame = new Game();
+        mehGame = new Game();
+
+        funnyGame.setPlayers(playersFunnyGame);
+        boringGame.setPlayers(playersBoringGame);
+        mehGame.setPlayers(playersMehGame);
+
 
         gameRepository = new GameRepository();
-        gameRepository.setGamesInfo(gamesInfoDtos);
+        gameRepository.addGame((long) 1, funnyGame);
+        gameRepository.addGame((long)2, boringGame);
+        gameRepository.addGame((long)3, mehGame);
 
     }
 
     @Test
     public void getGamesInfoByUserName()
     {
-        List<GameInfoDto> gamesBelongsToSebastian = new ArrayList<GameInfoDto>();
+        List<Game> gamesBelongsToSebastian = new ArrayList<Game>();
         gamesBelongsToSebastian.add(funnyGame);
         gamesBelongsToSebastian.add(mehGame);
 
-        Assert.assertEquals(gamesBelongsToSebastian, gameRepository.getGamesInfoByUserName("Sebastian"));
+        Assert.assertEquals(gamesBelongsToSebastian, gameRepository.getGames("Sebastian"));
     }
 
-    @Test
-    public void getGamesInfoByUserNameWithEmptyRepository()
-    {
-        List<GameInfoDto> emptyGames = new ArrayList<>();
-        gameRepository.setGamesInfo(emptyGames);
-
-        List<String> emptyUsernames = new ArrayList<>();
-
-        Assert.assertEquals(emptyUsernames, gameRepository.getGamesInfoByUserName("Sebastian"));
-    }
-     */
 }
