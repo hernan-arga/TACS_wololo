047a1e3fd162097a7834a32eb9e4d7e9a54b536f
hernan-arga <hernan_rodriguez_2013@hotmail.com>
2020-09-27 19:36:42 -0300

    Estadisticas municipality

---
+++ src/main/ui/src/app/_services/images.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ImagesService {
+
+  FUNCIONA = "https://cors-anywhere.herokuapp.com/https://pixabay.com/api/?key=18376709-a188ab6baf8fe12654f8c3700&q=Rojas&image_type=photo"
+
+  CORS_HEROKU = "https://cors-anywhere.herokuapp.com/"
+
+  // No subirlo en prod
+  API_KEY = '';
+
+  API_URL = 'https://pixabay.com/api/?key=';
+  API_NAME_PARAMETER = '&q=';
+  API_IMG_TYPE = '&image_type=photo';
+
+  constructor(private http: HttpClient) {
+  }
+  //this.CORS_HEROKU+this.API_URL+this.API_KEY+this.API_NAME_PARAMETER+imageSearch+this.API_IMG_TYPE
+
+  getImages(imageSearch: string): Observable<any> {
+    let imageSearchFormatted = imageSearch.split(" ").join('%20');
+
+    return this.http.get(
+      this.CORS_HEROKU+this.API_URL+this.API_KEY+this.API_NAME_PARAMETER+imageSearch+this.API_IMG_TYPE, 
+      { responseType: 'json' });
+  }
+  
+
+}
--- src/main/ui/src/app/game-show-municipality-statistics/game-show-municipality-statistics.component.ts
+++ src/main/ui/src/app/game-show-municipality-statistics/game-show-municipality-statistics.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Municipality } from '../shared/models/municipality.model';
+import { ImagesService } from '../_services/images.service';
 
 @Component({
   selector: 'app-game-show-municipality-statistics',
@@ -10,13 +11,27 @@ import { Municipality } from '../shared/models/municipality.model';
 export class GameShowMunicipalityStatisticsComponent implements OnInit {
 
   municipality: Municipality;
+  imageService: ImagesService;
+  municipalityImgUrl: string = '';
+  isLoading = true;
 
   constructor(public dialogRef: MatDialogRef<GameShowMunicipalityStatisticsComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Municipality) {
+    @Inject(MAT_DIALOG_DATA) public data: Municipality,
+    imageService: ImagesService) {
     this.municipality = data;
+    this.imageService = imageService;
   }
 
   ngOnInit(): void {
+    this.imageService.getImages(this.municipality.nombre).subscribe(
+      data => {
+        console.log(data);
+        /*this.municipalityImgUrl = data.hits[0].webformatURL; 
+      this.isLoading = false;*/},
+      err => {
+        console.log(err);
+      }
+    );
   }
 
   onAccept(): void {
