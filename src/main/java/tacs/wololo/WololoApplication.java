efbc882ee03937daaac24fadadb5d4625d089f40
hernan-arga <hernan_rodriguez_2013@hotmail.com>
2020-11-22 00:26:54 -0300

    avance en deploy

--- src/main/java/tacs/wololo/WololoApplication.java
+++ src/main/java/tacs/wololo/WololoApplication.java
@@ -4,29 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.zaxxer.hikari.*;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
-import javax.sql.DataSource;
 
 @SpringBootApplication
 @EnableScheduling
 public class WololoApplication {
-
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
 
     public static void main(String[] args)
     {
         SpringApplication.run(WololoApplication.class, args);
     }
 
-
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
-    }
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-INSERT INTO roles(name) values ('ROLE_USER');
+/*INSERT INTO roles(name) values ('ROLE_USER');
 INSERT INTO roles(name) values ('ROLE_ADMIN');
 
 INSERT INTO users(email, password, username)
     values ('admin@gmail.com', '$2y$12$W9J3f0WGNWJ9uAPcgUtocOCqWcFprMIF0dunTgmtwPrl6Pph9Tj6m','admin');
 
-INSERT INTO user_roles(user_id, role_id) values (1,2);
+INSERT INTO user_roles(user_id, role_id) values (1,2);*/
